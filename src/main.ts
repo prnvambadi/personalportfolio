d21e052880af02021875ecfa6443f39e8ef2fa05
Pranav Anilkumar <pranavragasudha@gmail.com>
2024-06-23 10:30:09 +0530

    enabled cross origin

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   dotenv.config(); 
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://pranavanilkumarportfolio.web.app'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  })
   await app.listen(3000);
 }
 bootstrap();
